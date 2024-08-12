b1441064ec5e814cd1f1614d02f8846f74bbe355
FARHAN AHMAD <117432911+789farhan@users.noreply.github.com>
2024-08-12 09:49:09 +0500

    responsivess

--- src/components/templates/homePage/features/FeaturesDescription.tsx
+++ src/components/templates/homePage/features/FeaturesDescription.tsx
@@ -7,42 +7,45 @@ import styled from "styled-components";
 import { featuresData } from "../../../../../Type";
 import { useState } from "react";
 
-
 export default function FeaturesDescription() {
-  const [align, setAlign] = useState("")
+  let [align, setAlign] = useState("row");
+  featuresData.map((data)=>{
+    if(data.id ==="odd"){
+      align="row-reverse"
+    }else{
+       align="row"
+    }
+    setAlign(align);
+  })
   const Box = styled.div`
-  display: flex;
+    display: flex;
+    flex-direction: ${align};
   `;
   const DeskcriptionBox = styled.div`
-  max-width: 40vw;
-  justify-content: center;
-  align-items: center;
-  
+    max-width: 40vw;
+    justify-content: center;
+    align-items: center;
   `;
   return (
     <>
-     {
-      featuresData.map((obj)=>{
-
-        return(
-          <Box>
-          <DeskcriptionBox>
-            <Heading
-            text={obj.heading}
-            textAlign="center"
-              fontSize="1.7rem"
-              fontWeight="600"
-              padding="0.7rem 0rem"
-              fontFamily={`Nexa Bold, sans-serif`}
-            />
-            <ParaGraph textAlign="center" fontSize="0.8rem" text={obj.para} />
-          </DeskcriptionBox>
-          <Image alt="" width={100} height={100} src={obj.gif}/>
-        </Box>
-        )
-      })
-     }
+      {featuresData.map((obj, index) => {
+        return (
+          <Box key={index}>
+            <DeskcriptionBox>
+              <Heading
+                text={obj.heading}
+                textAlign="center"
+                fontSize="1.7rem"
+                fontWeight="600"
+                padding="0.7rem 0rem"
+                fontFamily={`Nexa Bold, sans-serif`}
+              />
+              <ParaGraph textAlign="center" fontSize="0.8rem" text={obj.para} />
+            </DeskcriptionBox>
+            <Image alt="" width={100} height={100} src={obj.gif} />
+          </Box>
+        );
+      })}
     </>
   );
 }
- 
